1d6c09c7ad0ba3b0d1bfdc09d4ca06bca7b2bf7c
fabien-ricca <fabien.ricca@laplateforme.io>
2023-09-27 15:27:13 +0200

    Gestion erreurs - OK

--- MazeRunner/src/main/java/Main.java
+++ MazeRunner/src/main/java/Main.java
@@ -3,19 +3,22 @@ import java.util.ArrayList;
 public class Main {
 
     public static String checkArgs(String[] args){
+        String usage = "Utilisation : java -jar MazeRunner.jar [largeur] [hauteur] [perfect/imperfect] [simple/graph/optimized]";
 
+        // On vérifie que 4 arguments ont bien étaient passés
         if(args.length != 4){
-            return "Erreur : Veuillez fournir un type de labyrinthe et une méthode de génération valides.";
+            return "Erreur : Veuillez fournir un type de labyrinthe et une méthode de génération valides.\n" + usage;
         }
 
+        // On vérifie que les 2 premiers arguments sont biens des nombres, et qu'ils sont bien superieur à 5
         if(!checkNumber(args[0]) || !checkNumber(args[1])){
-            return "Erreur : Veuillez fournir une largeur et une hauteur valides supérieurs à 5.";
+            return "Erreur : Veuillez fournir une largeur et une hauteur valides supérieurs à 5.\n" + usage;
         }
 
-        if(!checkOption(args[2]) || !checkType(args[3])){
-            return "Erreur : Veuillez fournir un type de labyrinthe et une méthode de génération valides.";
+        // On vérifie que les 2 derniers arguments sont bien valides
+        if(!checkOption(args[2]) || !checkOption(args[3])){
+            return "Erreur : Veuillez fournir un type de labyrinthe et une méthode de génération valides.\n" + usage;
         }
-
 
         return "true";
     }
@@ -36,15 +39,7 @@ public class Main {
 
 
     public static boolean checkOption(String args){
-        if(!args.equals("perfect") || !args.equals("imperfect")){
-            return false;
-        }
-        return true;
-    }
-
-
-    public static boolean checkType(String args){
-        if(!args.equals("simple") && !args.equals("graph") && !args.equals("optimized")){
+        if(!args.equals("simple") && !args.equals("graph") && !args.equals("optimized") && !args.equals("perfect") && !args.equals("imperfect")){
             return false;
         }
         return true;
@@ -52,15 +47,40 @@ public class Main {
 
 
     public static void main(String[] args) {
-
-        System.out.println();
-        SimplePerfectMazeGenerator simplePerfect = new SimplePerfectMazeGenerator(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
-
         System.out.println();
         System.out.println();
-        SimpleImperfectMazeGenerator simpleImperfect = new SimpleImperfectMazeGenerator(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
 
+        if(checkArgs(args).equals("true")){
 
-//        System.out.println(checkArgs(args));
+            switch (args[2]){
+                case "perfect":
+                    switch (args[3]){
+                        case "simple":
+                            SimplePerfectMazeGenerator simplePerfect = new SimplePerfectMazeGenerator(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+                            break;
+
+                        case "graph":
+
+                            break;
+                    }
+                    break;
+
+                case "imperfect":
+                    switch (args[3]){
+                        case "simple":
+                            SimpleImperfectMazeGenerator simpleImperfect = new SimpleImperfectMazeGenerator(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+                            break;
+
+                        case "graph":
+
+                            break;
+                    }
+                    break;
+            }
+
+        }
+        else{
+            System.out.println(checkArgs(args));
+        }
     }
 }
